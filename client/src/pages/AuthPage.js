274f376b527516fd09c439e545a7b568e52806f6
unknown <macbaren124@gmail.com>
2020-03-18 12:13:43 +0300

    first commit

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import {useHttp} from '../hooks/http.hook'
+
+export const AuthPage = () => {
+  const {loading, request} = useHttp()
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form})
+      console.log('Data', data)
+    } catch (e) {}
+  }
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Less link...</h1>
+        <div className="card blue darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Authorisation</span>
+          <div>
+            <div className="input-field">
+              <input
+                placeholder="Enter email"
+                id="email"
+                type="text"
+                name="email"
+                className="yellow-input"
+                onChange={changeHandler}
+              />
+              <label for="email">Email</label>
+            </div>
+            <div className="input-field">
+              <input
+                placeholder="Enter password"
+                id="password"
+                type="password"
+                name="password"
+                className="yellow-input"
+                onChange={changeHandler}
+              />
+              <label for="password">Password</label>
+            </div>
+          </div>
+        </div>
+        <div className="card-action">
+          <button
+            className="btn yellow darken-4"
+            disabled={loading}
+          >
+            Enter
+          </button>
+          <button
+            className="btn grey lighten-1 black-text"
+            onClick={registerHandler}
+            disabled={loading}
+          >
+            Regitstration
+          </button>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
